4a567f1920fd502cc38491693767f0b2c7a3d546
Maximilian Held <info@maxheld.de>
2025-03-23 21:32:42 +0100

    remove boilerplate comments

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,4 +1,3 @@
-# flake.nix
 {
   description = "The template flake";
   
@@ -6,16 +5,12 @@
 
   outputs = { self, nixpkgs, systems, treefmt-nix }:
     let
-      # Small tool to iterate over each systems
       eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
 
-      # Eval the treefmt modules from ./treefmt.nix
       treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs { programs.mdformat.enable = true; });
     in
     {
-      # for `nix fmt`
       formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-      # for `nix flake check`
       checks = eachSystem (pkgs: {
         formatting = treefmtEval.${pkgs.system}.config.build.check self;
       });
